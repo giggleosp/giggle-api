6fb8dd76cbd933fccf10884e4bbb8d69efde7de4
Enda Phelan <Enda Phelan>
2015-11-22 22:48:28 +0000

    First commit

---
+++ src/main/java/com/giggle/GiggleApplication.java
@@ -0,0 +1,12 @@
+package com.giggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiggleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/giggle/GiggleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.giggle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GiggleApplication.class)
+@WebAppConfiguration
+public class GiggleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
